f74962d35e8cf9c1f7f114f5032b518d8b967557
dirgh8yu <dirgh8yu@gmail.com>
2026-08-20 13:35:51 +1000

    Type the shipment documents R2 binding

--- cloudflare-env.d.ts
+++ cloudflare-env.d.ts
@@ -3,6 +3,7 @@
 declare global {
   interface CloudflareEnv {
     DB: D1Database;
+    DOCUMENTS?: R2Bucket;
     KCPL_ADMIN_PASSWORD?: string;
     KCPL_ADMIN_SESSION_SECRET?: string;
     KCPL_ADMIN_NAME?: string;
