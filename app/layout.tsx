8e101d62ca0eede4b48303f721b391c9d26f3965
dirgh8yu <dirgh8yu@gmail.com>
2026-09-14 09:46:50 +1000

    load final Commercial refinement after legacy styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ import "./admin/operations-mobile.css";
 import "./admin/operations-action-hierarchy.css";
 import "./admin/operations-detail-refinement.css";
 import "./admin/commercial-v4-compat.css";
+import "./admin/commercial-detail-refinement.css";
 
 const geist = Geist({
   variable: "--font-geist",
